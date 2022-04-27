9d1e7b90a5ae94480c7f1960d3a9d0d01455f64f
Ilay <ilay22rus@mail.ru>
2022-04-27 21:21:18 +0700

    hhh

---
+++ task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ввести первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввести второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine($"Максимальное число {numberA} минимальное число {numberB}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {numberB} минимальное число {numberA}");
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Вводим первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Вводим второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Вводим третье число: ");
+int numberC = int.Parse(Console.ReadLine());
+if (numberA > numberB & numberA > numberC)
+{
+    Console.WriteLine($"Число {numberA} наибольшее число");
+}
+if (numberB > numberA & numberB > numberC)
+{
+    Console.WriteLine($"Число {numberB} наибольшее число");
+}
+if (numberC > numberA & numberC > numberB)
+{
+    Console.WriteLine($"Число {numberC} наибольшее число");
+}
+
+//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
